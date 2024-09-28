47effc2b35f3e2b7b4a4ea486dcde4a15b8d2322
ZIECOZENO4 <ziecozeno4@gmail.com>
2024-09-28 22:39:35 +0100

    nb ghv

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -56,7 +56,7 @@ export function HeroSection() {
   const profile = profiles[currentProfile]
   return (
     <div className="flex justify-center items-center h-auto mt-4">
-    {/* <BackgroundGradient className="flex flex-col rounded-[22px] w-full align-middle items-center p-4 sm:p-10 bg-zinc-900">
+    <BackgroundGradient className="flex flex-col rounded-[22px] w-full align-middle items-center p-4 sm:p-10 bg-zinc-900">
   <StartingButton  />
     <div className="flex align-middle my-6 flex-col sm:flex-row items-center space-y-4 sm:space-y-0 sm:space-x-4">
           <motion.div 
@@ -105,57 +105,7 @@ export function HeroSection() {
             </div>
 
         </div>
-      </BackgroundGradient> */}
-          <div className="flex flex-col rounded-[22px] w-full align-middle items-center p-4 sm:p-10 bg-zinc-900">
-  <StartingButton  />
-    <div className="flex align-middle my-6 flex-col sm:flex-row items-center space-y-4 sm:space-y-0 sm:space-x-4">
-          <motion.div 
-            className="relative w-32 md:w-40 md:h-40 h-32 rounded-full overflow-hidden"
-            animate={{ y: [0, -10, 0] }}
-            transition={{ repeat: Infinity, duration: 2 }}
-          >
-            <img
-              alt="Profile picture"
-              className="object-cover"
-              src={profile.image}
-            />
-            <div className="absolute inset-0 rounded-full border-2 border-blue-400 opacity-50"></div>
-          </motion.div>
-          <motion.div 
-            className="text-center sm:text-left"
-            key={profile.name}
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            exit={{ opacity: 0 }}
-            transition={{ duration: 0.5 }}
-          >
-            <h2 className="text-2xl md:text-3xl text-light  text-white">Created by {profile.name}</h2>
-            <div className="flex flex-wrap justify-center sm:justify-start gap-2 mt-2 text-white">
-              {profile.badges.map((badge, index) => (
-                <Chip key={index} color={`${index % 2 === 0 ? 'default' : 'warning'}`} variant={`${index % 2 === 0 ? 'shadow' : 'dot'}`} className='font-bold'>
-                  {badge}
-                </Chip>
-              ))}
-            </div>
-            <p className="mt-2 text-gray-500 flex">Market Cap: <span className='ml-2 text-white'>{profile.marketCap}</span> </p>
-            <p className="text-gray-500 flex">Replies:<span className='ml-2 text-white'> {profile.replies}</span> </p>
-            <p className="text-slate-500 mt-2">
-              {profile.tickerName} [ticker: {profile.ticker}]
-            </p>
-            <Chip color="default" className='text-sm md:text-md my-1 font-bold'>{profile.address}</Chip>
-          </motion.div>
-        </div>
-        <div className="gap-4 my-2 flex flex-col w-full md:flex-row ">
-          <div className="md:w-[70%] w-full">
-<HeroSearchInput />
-          </div>
-
-<div className="md:w-[30%] w-full">
-<ViewMoreButton />            
-            </div>
-
-        </div>
-      </div>
+      </BackgroundGradient>
     </div>
   );
 }
