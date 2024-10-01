bdb6ae2f62f674da7d2e12b8b36a08bdf28123e2
ZIECOZENO4 <ziecozeno4@gmail.com>
2024-10-01 17:13:07 +0100

    ryrty

--- components/CardDemo.tsx
+++ components/CardDemo.tsx
@@ -45,26 +45,28 @@ const CardDemo = () => {
                   [SPEAR]
                 </motion.h2>
                 <motion.h2
-                  className="hover:underline text-[#F7F2DA]"
-                  style={{
-                    width: '20px',
-                    height: '10px',
-                  
-                    top: '9px',
-                    left: '302px',
-                
-                    fontSize: '10px',
-                    fontWeight: 200,
-                    lineHeight: '20px',
-                    textAlign: 'left',
-                    color: '#F7F2DA',
-                  }}
-                  initial={{ opacity: 0, y: -20 }}
-                  animate={{ opacity: 1, y: 0 }}
-                  transition={{ delay: 0.2, duration: 0.5 }}
-                >
-                  More
-                </motion.h2>
+  className="hover:underline text-[#F7F2DA] workbench-text"
+  style={{
+    width: '20px',
+    height: '10px',
+    top: '9px',
+    left: '302px',
+    fontSize: '10px',
+    fontWeight: 200,
+    lineHeight: '20px',
+    textAlign: 'left',
+    color: '#F7F2DA',
+    fontFamily: '"Workbench", system-ui',
+    fontOpticalSizing: 'auto',
+    fontStyle: 'normal',
+    fontVariationSettings: '"BLED" 0, "SCAN" -53'
+  }}
+  initial={{ opacity: 0, y: -20 }}
+  animate={{ opacity: 1, y: 0 }}
+  transition={{ delay: 0.2, duration: 0.5 }}
+>
+  More
+</motion.h2>
               </div>
               <div className="flex flex-col  align-middle">
                 <motion.p
