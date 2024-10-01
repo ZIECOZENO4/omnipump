3927249798fa2a7119cb4cae2017a59ecddd6d06
ZIECOZENO4 <ziecozeno4@gmail.com>
2024-10-01 15:03:02 +0100

    jhyguhj

--- components/CardDemo.tsx
+++ components/CardDemo.tsx
@@ -1,20 +1,20 @@
-"use client";
-import React from "react";
-import { motion } from "framer-motion";
+"use client"
+import React from 'react'
+import { motion } from 'framer-motion'
 
 const CardDemo = () => {
   return (
     <div className="justify-center items-center align-middle grid grid-cols-12 gap-4 no-scrollbar">
       <motion.div
         style={{
-          width: "332px",
-          height: "150px",
-
-          top: "1022px",
-          left: "3542px",
-          background: "#0A0909",
-          borderRadius: "8px", // Assuming you want to keep the rounded corners
-          overflow: "hidden"
+          width: '332px',
+          height: '150px',
+        
+          top: '1022px',
+          left: '3542px',
+          background: '#0A0909',
+          borderRadius: '8px', // Assuming you want to keep the rounded corners
+          overflow: 'hidden',
         }}
         initial={{ opacity: 0, scale: 0.9 }}
         animate={{ opacity: 1, scale: 1 }}
@@ -22,21 +22,21 @@ const CardDemo = () => {
       >
         <div className="relative p-3 text-white">
           <div className="flex justify-between items-start">
-            <div className="w-[100px] h-[100px] my-[10px] mx-[10px] bg-[#D9D9D966]" />
-
+            <div  className="w-[100px] h-[100px] my-[10px] mx-[10px] bg-[#D9D9D966]" />
+            
             <div className="text-right flex flex-col p-2">
               <div className="flex flex-row justify-between align-middle">
                 <motion.h2
                   className="text-left text-[#F7F2DA] font-normal"
                   style={{
-                    width: "70px",
-                    height: "20px",
-
-                    top: "14px",
-                    left: "137px",
-
-                    fontSize: "20px",
-                    lineHeight: "20px"
+                    width: '70px',
+                    height: '20px',
+                  
+                    top: '14px',
+                    left: '137px',
+                
+                    fontSize: '20px',
+                    lineHeight: '20px',
                   }}
                   initial={{ opacity: 0, y: -20 }}
                   animate={{ opacity: 1, y: 0 }}
@@ -45,39 +45,39 @@ const CardDemo = () => {
                   [SPEAR]
                 </motion.h2>
                 <motion.h2
-                  className="hover:underline text-[#F7F2DA] cursor-pointer"
+                  className="hover:underline text-[#F7F2DA]"
                   style={{
-                    width: "auto",
-                    height: "10px",
-                    top: "9px",
-                    left: "302px",
-                    fontSize: "9px",
-                    fontWeight: 300,
-                    lineHeight: "10px",
-                    textAlign: "left",
-                    color: "#F7F2DA",
-                    opacity: 0.9,
-                    textShadow: "0 0 1px rgba(247, 242, 218, 0.3)"
+                    width: '20px',
+                    height: '10px',
+                  
+                    top: '9px',
+                    left: '302px',
+                
+                    fontSize: '10px',
+                    fontWeight: 400,
+                    lineHeight: '5px',
+                    textAlign: 'left',
+                    color: '#F7F2DA',
                   }}
                   initial={{ opacity: 0, y: -20 }}
                   animate={{ opacity: 1, y: 0 }}
                   transition={{ delay: 0.2, duration: 0.5 }}
                 >
-                  more
+                  More
                 </motion.h2>
               </div>
               <div className="flex flex-col  align-middle">
                 <motion.p
                   className="text-[#F7F2DA] flex flex-row mt-[5px]"
                   style={{
-                    width: "60px",
-                    height: "10px",
-                    left: "137px",
-                    fontSize: "10px",
+                    width: '60px',
+                    height: '10px',
+                    left: '137px',
+                    fontSize: '10px',
                     fontWeight: 200,
-                    lineHeight: "10px",
-                    textAlign: "left",
-                    color: "#F7F2DA"
+                    lineHeight: '10px',
+                    textAlign: 'left',
+                    color: '#F7F2DA',
                   }}
                   initial={{ opacity: 0, y: -10 }}
                   animate={{ opacity: 1, y: 0 }}
@@ -85,18 +85,18 @@ const CardDemo = () => {
                 >
                   Pear Network
                 </motion.p>
-
-                <motion.div
-                  className="mt-[25px]"
+          
+                       <motion.div
+                       className='mt-[25px]'
                   style={{
-                    width: "180px",
-                    height: "10px",
-
-                    top: "85px",
-                    left: "137px",
-                    display: "flex",
-                    justifyContent: "space-between",
-                    alignItems: "center"
+                    width: '180px',
+                    height: '10px',
+                  
+                    top: '85px',
+                    left: '137px',
+                    display: 'flex',
+                    justifyContent: 'space-between',
+                    alignItems: 'center',
                   }}
                   initial={{ opacity: 0, x: -20 }}
                   animate={{ opacity: 1, x: 0 }}
@@ -104,38 +104,40 @@ const CardDemo = () => {
                 >
                   <p
                     style={{
-                      fontSize: "10px",
+                  
+                      fontSize: '10px',
                       fontWeight: 400,
-                      lineHeight: "10px",
-                      textAlign: "left",
-                      color: "#F7F2DA"
+                      lineHeight: '10px',
+                      textAlign: 'left',
+                      color: '#F7F2DA',
                     }}
                   >
-                    Time to Launch:
+                  Time to Launch:
                   </p>
                   <p
                     style={{
-                      fontSize: "12px",
+                  
+                      fontSize: '12px', 
                       fontWeight: 400,
-                      lineHeight: "10px",
-                      textAlign: "left",
-                      color: "#F7F2DA"
+                      lineHeight: '10px',
+                      textAlign: 'left',
+                      color: '#F7F2DA',
                     }}
                   >
-                    00D/4H/24m
+                  00D/4H/24m
                   </p>
                 </motion.div>
                 <motion.div
-                  className="my-[8px]"
+                   className='my-[8px]'
                   style={{
-                    width: "180px",
-                    height: "10px",
-
-                    top: "85px",
-                    left: "137px",
-                    display: "flex",
-                    justifyContent: "space-between",
-                    alignItems: "center"
+                    width: '180px',
+                    height: '10px',
+                  
+                    top: '85px',
+                    left: '137px',
+                    display: 'flex',
+                    justifyContent: 'space-between',
+                    alignItems: 'center',
                   }}
                   initial={{ opacity: 0, x: -20 }}
                   animate={{ opacity: 1, x: 0 }}
@@ -143,41 +145,43 @@ const CardDemo = () => {
                 >
                   <p
                     style={{
-                      fontSize: "10px",
+                  
+                      fontSize: '10px',
                       fontWeight: 400,
-                      lineHeight: "10px",
-                      textAlign: "left",
-                      color: "#F7F2DA"
+                      lineHeight: '10px',
+                      textAlign: 'left',
+                      color: '#F7F2DA',
                     }}
                   >
                     Chain:
                   </p>
                   <p
                     style={{
-                      fontSize: "10px",
+                  
+                      fontSize: '10px',
                       fontWeight: 400,
-                      lineHeight: "10px",
-                      textAlign: "left",
-                      color: "#F7F2DA"
+                      lineHeight: '10px',
+                      textAlign: 'left',
+                      color: '#F7F2DA',
                     }}
                   >
                     Arbitrum
                   </p>
                 </motion.div>
                 <motion.div
-                  className=" mb-[8px]"
+                   className=' mb-[8px]'
                   style={{
-                    width: "180px",
-                    height: "10px",
-
-                    top: "102px",
-                    left: "137px",
-
-                    fontSize: "10px",
+                    width: '180px',
+                    height: '10px',
+                  
+                    top: '102px',
+                    left: '137px',
+                
+                    fontSize: '10px',
                     fontWeight: 400,
-                    lineHeight: "10px",
-                    textAlign: "left",
-                    color: "#F7F2DA"
+                    lineHeight: '10px',
+                    textAlign: 'left',
+                    color: '#F7F2DA',
                   }}
                   initial={{ opacity: 0, x: -20 }}
                   animate={{ opacity: 1, x: 0 }}
@@ -191,7 +195,7 @@ const CardDemo = () => {
         </div>
       </motion.div>
     </div>
-  );
-};
+  )
+}
 
-export default CardDemo;
+export default CardDemo
