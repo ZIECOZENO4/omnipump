94c6979a49ff6be00c10cfbfb265af628c46e916
ZIECOZENO4 <ziecozeno4@gmail.com>
2024-10-03 09:01:05 +0100

    pk'pokokm

--- components/CardDemo.tsx
+++ components/CardDemo.tsx
@@ -385,8 +385,8 @@ const Card = () => (
 
 const CardDemo = () => {
   return (
-    <div className="w-full  px-4 md:px-20 " >
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 md:gap-10">
+    <div className="w-full  px-4 md:px-48 " >
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 md:gap-20">
         {[...Array(50)].map((_, index) => (
           <Link href='/omnicoin' key={index}>
             <Card />
