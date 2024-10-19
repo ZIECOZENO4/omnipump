3ccbc170999ef8ca2961425e9c045c18d609318f
ZIECOZENO4 <ziecozeno4@gmail.com>
2024-10-19 15:19:14 +0100

    jblkljhj

--- components/VideoBackground.tsx
+++ components/VideoBackground.tsx
@@ -20,7 +20,7 @@ const BackgroundVideo: React.FC = () => {
         muted
         loop
         playsInline
-        className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 min-w-[100%] min-h-[100%] w-auto h-auto object-cover"
+        className="w-[100vw] h-[100vh] object-cover"
       >
         <source src="/videos/first.mp4" type="video/mp4" />
         <source src="/videos/first.webm" type="video/webm" />
